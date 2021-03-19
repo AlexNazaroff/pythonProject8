0bde0d624dae6793cf51ecb53888a7e7a073eb9f
AlexNazaroff <ale.krotov@gmail.com>
2021-03-19 13:45:58 +0500

    commit 2

---
+++ 8.py
@@ -0,0 +1,25 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+#driver = webdriver.Firefox
+
+driver=webdriver.Chrome()
+#wait = WebDriverWait(driver, 20)
+
+#driver.implicitly_wait(20) # seconds
+
+#driver.get("http://somedomain/url_that_delays_loading")
+#myDynamicElement = driver.find_element_by_id("myDynamicElement")
+
+driver.get('http://www.python.org')
+
+driver.find_element_by_link_text("Documentation").click()
+driver.find_element_by_link_text("Docs").click()
+driver.find_element_by_link_text("index").click()
+driver.find_element_by_link_text("Python").click()
+driver.find_element_by_link_text("Documentation").click()
+driver.find_element_by_link_text("FAQ").click()
+driver.find_element_by_link_text("Python").click()
+#driver.find_element_by_name("System Administration:").send_keys("Ansible")
